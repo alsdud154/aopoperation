e810d7ca24e5fb8839b7b34621d1674fc19d79df
정민영 <alsdud151350@gmail.com>
2021-05-05 11:49:57 +0900

    초기 설정

---
+++ src/main/java/kr/co/velnova/aopoperation/AopoperationApplication.java
@@ -0,0 +1,13 @@
+package kr.co.velnova.aopoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopoperationApplication.class, args);
+	}
+
+}
